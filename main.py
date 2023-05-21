b2423e0659528be0daef69af4656166d41a806ab
Daniel Porto <danielportods@gmail.com>
2023-05-21 17:26:56 -0300

    Falsifica funcão valida_campo e ajustes Co-authored-by: AlGouvea <alvarohsgouvea@gmail.com> Co-authored-by: Carlacangussu <karlacangussu@hotmail.com> Co-authored-by: Hugorochaffs <unb.hugorochaffs@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import json
 
 def valida_registro(path):
-    return [100, 60, 0]
+    return [100, 60, 0] 
+
+def valida_campo(chave, valor):
+    return 1
+
 
 if __name__ == '__main__':
     print(valida_registro('files/test_files/hw.json'))
+    
+    #parte para auxiliar na implementacao
+    #ret = valida_registro('files/test_files/registro.json')
+
+    #for i in ret:
+        #for chave in ret[i]:
+            #print(chave)
--- test_main.py
+++ test_main.py
@@ -1,6 +1,14 @@
-from main import valida_registro
+from main import valida_registro, valida_campo
 
 def test_valida_registro():
     path = 'files/test_files/registro.json'
     registro = valida_registro(path)
     assert registro == [100, 60, 0]
+
+
+def test_valida_campo():
+    chave = 'cpf'
+    valor = '09999999999'
+    res = valida_campo(chave, valor)
+
+    assert res == 1 
