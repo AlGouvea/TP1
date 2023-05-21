75497e1a6f259e35888d6e28a691893298f5ab09
Daniel Porto <danielportods@gmail.com>
2023-05-21 19:00:48 -0300

    Triangulação com teste parametrizado da funcao valida_campo Co-authored-by: Carlacangussu <karlacangussu@hotmail.com> Co-authored-by: AlGouvea <alvarohsgouvea@gmail.com>

--- test_main.py
+++ test_main.py
@@ -1,5 +1,9 @@
 from main import valida_registro, valida_campo
 
+PARAMETHER = [[{"cpf": ""}, 0],
+              [{"Nome": {"PrimeiroNome": "João", "NomeMeio": "", "UltimoNome": "Silva"}}, 1], 
+              [{"Nome": {"PrimeiroNome": "", "NomeMeio": "", "UltimoNome": ""}}, 0]]
+
 def test_valida_registro():
     path = 'files/test_files/registro.json'
     registro = valida_registro(path)
@@ -14,8 +18,25 @@ def test_valida_campo_cpf():
     assert res == 1 
 
 def test_valida_campo_nome():
-    chave = 'nome'
+    chave = 'Nome'
     valor = {"PrimeiroNome": "João", "NomeMeio": "", "UltimoNome": "Silva"}
     res = valida_campo(chave, valor)
 
     assert res == 1
+
+def test_valida_campo_nome_vazio():
+    chave = 'Nome'
+    valor = {"PrimeiroNome": "", "NomeMeio": "", "UltimoNome": ""}
+    res = valida_campo(chave, valor)
+
+    assert res == 0
+
+def valida_campo_parametrizado(parametros):
+    dictionary_keys = [list(inner_list[0].keys())[0] for inner_list in PARAMETHER if isinstance(inner_list[0], dict)]
+    dictionary_values = [list(inner_list[0].values())[0] for inner_list in PARAMETHER if isinstance(inner_list[0], dict)]
+
+    for k, v, p in zip(dictionary_keys, dictionary_values, parametros):
+        assert valida_campo(k, v) == p[1]
+
+def test_parametrizado():
+    valida_campo_parametrizado(PARAMETHER)
